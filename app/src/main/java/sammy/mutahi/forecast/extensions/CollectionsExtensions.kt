cbe21dd58dd7a2dc7b73f05015d85fcf265444a1
sammymutahigicheru <sammymutahi12@gmail.com>
2019-08-22 11:26:44 +0300

    modified collections extension file

--- app/src/main/java/sammy/mutahi/forecast/extensions/CollectionsExtensions.kt
+++ app/src/main/java/sammy/mutahi/forecast/extensions/CollectionsExtensions.kt
@@ -1,7 +1,17 @@
 package sammy.mutahi.forecast.extensions
 
+/*
+* This extension pairs key and values
+* 
+*
+* */
+
 fun <K, V : Any> Map<K, V?>.toVarargArray(): Array<out Pair<K, V>> =
     map { Pair(it.key, it.value!!) }.toTypedArray()
+
+/*
+* iterates through db,if a match is found return result else throws an exception
+* */
 
 inline fun <T, R:Any>Iterable<T>.firstResult(predicate:(T)->R?):R{
     for(element in this){
