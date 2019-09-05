dd9d9b014b35954c0d6d201c61fdcda70b6a5218
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-05 11:02:52 +0300

    added request forecast command class

---
+++ app/src/main/java/sammy/mutahi/weatherforecast/domain/commands/RequestForecastCommand.kt
@@ -0,0 +1,13 @@
+package sammy.mutahi.weatherforecast.domain.commands
+
+import sammy.mutahi.weatherforecast.data.server.ForecastRequest
+import sammy.mutahi.weatherforecast.domain.mapper.ForecastDataMapper
+import sammy.mutahi.weatherforecast.domain.model.ForecastList
+
+class RequestForecastCommand(val cityName:String):Command<ForecastList> {
+    override fun execute(): ForecastList {
+        val forecastRequest = ForecastRequest(cityName)
+        return ForecastDataMapper().convertFromDataModel(forecastRequest.execute())
+    }
+
+}
