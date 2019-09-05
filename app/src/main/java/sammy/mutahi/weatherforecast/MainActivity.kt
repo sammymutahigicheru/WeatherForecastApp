aaceb99feca1a36284dd285e5194388487150c2b
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-05 08:54:51 +0300

    modified mainactivivity

--- app/src/main/java/sammy/mutahi/weatherforecast/MainActivity.kt
+++ app/src/main/java/sammy/mutahi/weatherforecast/MainActivity.kt
@@ -4,12 +4,25 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import sammy.mutahi.weatherforecast.adapters.ForecastListAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    //dummy-data
+    private val items = listOf(
+        "Mon 6/23 - Sunny - 31/17",
+        "Tue 6/24 - Foggy - 21/8",
+        "Wed 6/25 - Cloudy - 22/17",
+        "Thurs 6/26 - Rainy - 18/11",
+        "Fri 6/27 - Foggy - 21/10",
+        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+        "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         forecast_list.layoutManager = LinearLayoutManager(this)
+        forecast_list.adapter = ForecastListAdapter(items)
     }
 }
