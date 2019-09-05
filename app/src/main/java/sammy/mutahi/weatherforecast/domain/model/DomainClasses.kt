d3d83be793848b16d9d16a935c0ff31c1273fbed
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-05 10:16:14 +0300

    add domain classes

---
+++ app/src/main/java/sammy/mutahi/weatherforecast/domain/model/DomainClasses.kt
@@ -0,0 +1,13 @@
+package sammy.mutahi.weatherforecast.domain.model
+
+/*
+* The first command will request data to API and convert it to these domain classes
+* */
+
+data class ForecastList(
+    val city: String, val country: String,
+    val dailyForecast: List<Forecast>
+)
+
+data class Forecast(val date: String, val description: String, val high: Int,
+                    val low: Int)
