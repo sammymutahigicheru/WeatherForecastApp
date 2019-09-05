229737452061b4e38c17eb5a06b4e7cf60d97c16
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-05 08:52:17 +0300

    created forecastlistadapter

--- app/src/main/java/sammy/mutahi/weatherforecast/adapters/ForecastListAdapter.kt
+++ app/src/main/java/sammy/mutahi/weatherforecast/adapters/ForecastListAdapter.kt
@@ -1,4 +1,20 @@
 package sammy.mutahi.weatherforecast.adapters
 
-class ForecastListAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+class ForecastListAdapter(val items: List<String>) :
+    RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        return ViewHolder(TextView(p0.context))
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        p0.textView.text = items[p1]
+    }
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
