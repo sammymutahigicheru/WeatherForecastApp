33201222c1f03de86e9610feafd3f55d1dcfbbc5
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-05 13:31:19 +0300

    added sqlite tables

---
+++ app/src/main/java/sammy/mutahi/weatherforecast/data/sqliteDatabase/Tables.kt
@@ -0,0 +1,17 @@
+package sammy.mutahi.weatherforecast.data.sqliteDatabase
+
+object CityForecastTable{
+    const val NAME = "CityForecast"
+    const val CITY = "city"
+    const val COUNTRY = "country"
+
+}
+object DayForecastTable {
+    const val NAME = "DayForecast"
+    const val ID = "_id"
+    const val DATE = "date"
+    const val DESCRIPTION = "description"
+    const val HIGH = "high"
+    const val LOW = "low"
+    const val ICON_URL = "iconUrl"
+}
