03f2a685b36cae2c290ba2c04e9a28c7ecd53ea4
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-05 09:19:19 +0300

    perfomed api request in main Activity

--- app/src/main/java/sammy/mutahi/weatherforecast/activities/MainActivity.kt
+++ app/src/main/java/sammy/mutahi/weatherforecast/activities/MainActivity.kt
@@ -4,26 +4,31 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 import sammy.mutahi.weatherforecast.R
 import sammy.mutahi.weatherforecast.adapters.ForecastListAdapter
+import sammy.mutahi.weatherforecast.data.Request
 
 class MainActivity : AppCompatActivity() {
 
-    //dummy-data
-    private val items = listOf(
-        "Mon 6/23 - Sunny - 31/17",
-        "Tue 6/24 - Foggy - 21/8",
-        "Wed 6/25 - Cloudy - 22/17",
-        "Thurs 6/26 - Rainy - 18/11",
-        "Fri 6/27 - Foggy - 21/10",
-        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-        "Sun 6/29 - Sunny - 20/7"
-    )
+    /*
+    * open weather api
+    * */
+    val url =
+        "http://api.openweathermap.org/data/2.5/forecast/daily?APPID=15646a06818f61f7b8d7823ca833e1ce&q=mombasa&mode=json&units=metri\\%20c&cnt=7"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         forecast_list.layoutManager = LinearLayoutManager(this)
-        forecast_list.adapter = ForecastListAdapter(items)
+        doAsync {
+            //perform request
+            Request(url).run()
+            uiThread {
+                toast("Request Performed")
+            }
+        }
     }
 }
