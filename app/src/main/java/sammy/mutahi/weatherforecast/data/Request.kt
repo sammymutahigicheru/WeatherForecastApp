cd7b778d4640f6fba33cf4516927d56a1bade6d1
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-05 09:11:12 +0300

    add request class in data package

---
+++ app/src/main/java/sammy/mutahi/weatherforecast/data/Request.kt
@@ -0,0 +1,12 @@
+package sammy.mutahi.weatherforecast.data
+
+import android.util.Log
+import java.net.URL
+
+class Request(val url:String){
+    fun run(){
+        val forecastJsonStr = URL(url).readText()
+        Log.d(javaClass.simpleName,forecastJsonStr)
+
+    }
+}
