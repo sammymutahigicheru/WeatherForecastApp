9f5ded4c725f02fa6916062535c800d035174cfe
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-06 10:05:32 +0300

    modified activity settings layout

--- app/src/main/java/sammy/mutahi/weatherforecast/ui/activities/MainActivity.kt
+++ app/src/main/java/sammy/mutahi/weatherforecast/ui/activities/MainActivity.kt
@@ -3,21 +3,32 @@ package sammy.mutahi.weatherforecast.ui.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 import sammy.mutahi.weatherforecast.R
 import sammy.mutahi.weatherforecast.domain.commands.RequestForecastCommand
 import sammy.mutahi.weatherforecast.ui.adapters.ForecastListAdapter
+import sammy.mutahi.weatherforecast.ui.utils.ToolBarManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    ToolBarManager {
+    override val toolbar: Toolbar by lazy {
+        find<Toolbar>(R.id.toolbar)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
+
 
         forecast_list.layoutManager = LinearLayoutManager(this)
+        attachToScroll(forecast_list)
 
         doAsync {
             val result = RequestForecastCommand("mombasa").execute()
