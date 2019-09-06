63b53f8b7080868f9b947687a71f86e7d08102e1
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-06 09:16:11 +0300

    create settings activity

---
+++ app/src/main/java/sammy/mutahi/weatherforecast/ui/activities/Settings.kt
@@ -0,0 +1,13 @@
+package sammy.mutahi.weatherforecast.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import sammy.mutahi.weatherforecast.R
+
+class Settings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
