e246735b5b9cb367c7e1ae1a27e4958417370a08
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-06 10:16:41 +0300

    create string preference in DelegatesExt

--- app/src/main/java/sammy/mutahi/weatherforecast/ui/utils/extensions/DelegatesExt.kt
+++ app/src/main/java/sammy/mutahi/weatherforecast/ui/utils/extensions/DelegatesExt.kt
@@ -1,9 +1,12 @@
 package sammy.mutahi.weatherforecast.ui.utils.extensions
 
+import android.content.Context
 import kotlin.reflect.KProperty
 
 object DelegatesExt {
     fun <T> notNullSingleValue() = NotNullSingleValueVar<T>()
+    fun stringPreference(context: Context,name: String,default: String)
+    =StringPreference(context,name,default)
 }
 
 class NotNullSingleValueVar<T> {
@@ -16,5 +19,19 @@ class NotNullSingleValueVar<T> {
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
         this.value = if (this.value == null) value
         else throw IllegalStateException("${property.name} already initialized")
+    }
+}
+
+/*
+*queries preference when get is called and saves it when set is called
+* */
+class StringPreference(val ctx: Context, val name: String, val default: String) {
+    val prefs by lazy {
+        ctx.getSharedPreferences("default", Context.MODE_PRIVATE)
+    }
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String? = prefs.getString(name, default)
+    operator fun setValue(thisRef: Any?, property: KProperty<*>,value:String){
+        prefs.edit().putString(name,value).apply()
     }
 }
