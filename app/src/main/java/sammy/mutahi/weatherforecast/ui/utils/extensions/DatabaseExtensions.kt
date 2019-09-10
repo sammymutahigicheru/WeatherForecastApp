a9e6f69da54b8330696c1db7f5c1644349c91df3
sammymutahigicheru <sammymutahi12@gmail.com>
2019-09-10 15:13:38 +0300

    modified db extensions by adding parselist functionality

--- app/src/main/java/sammy/mutahi/weatherforecast/ui/utils/extensions/DatabaseExtensions.kt
+++ app/src/main/java/sammy/mutahi/weatherforecast/ui/utils/extensions/DatabaseExtensions.kt
@@ -1,2 +1,11 @@
 package sammy.mutahi.weatherforecast.ui.utils.extensions
 
+import org.jetbrains.anko.db.MapRowParser
+import org.jetbrains.anko.db.SelectQueryBuilder
+
+//returns map row parser which uses columns as keys
+fun <T : Any> SelectQueryBuilder.parseList(
+    parser: (Map<String, Any?>) -> T): List<T> =
+    parseList(object : MapRowParser<T> {
+        override fun parseRow(columns: Map<String, Any?>): T = parser(columns)
+    })
